7aa4841ab36c34051d7e56ae94ecec222be9d481
Orlando MG <omoralesg2000@alumno.ipn.mx>
2026-02-08 12:19:11 -0600

    feat: add roles and permissions (rbac)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from app.core.config import settings
+
 from app.modules.auth.router import router as auth_router
 from app.modules.users.router import router as users_router
-from app.db.session import SessionLocal
-from app.db.seeds.initial_data import run_seeds
 from app.modules.products.router import router as products_router
 from app.modules.inventory.router import router as inventory_router
 from app.modules.suppliers.router import router as suppliers_router
@@ -11,7 +10,6 @@ from app.modules.purchases.router import router as purchases_router
 from app.modules.finance.router import router as finance_router
 from app.modules.sales.router import router as sales_router
 from app.modules.reports.router import router as reports_router
-
 
 app = FastAPI(
     title=settings.app_name,
@@ -21,20 +19,19 @@ app = FastAPI(
     }
 )
 
+# --------------------------------------------------
+# Healthcheck
+# --------------------------------------------------
 @app.get("/health", tags=["Health"])
 def health_check():
     return {"status": "ok"}
+
+
+# --------------------------------------------------
+# Routers
+# --------------------------------------------------
 app.include_router(auth_router)
 app.include_router(users_router)
-
-@app.on_event("startup")
-def startup_event():
-    db = SessionLocal()
-    try:
-        run_seeds(db)
-    finally:
-        db.close()
-
 app.include_router(products_router)
 app.include_router(inventory_router)
 app.include_router(suppliers_router)
@@ -42,4 +39,17 @@ app.include_router(purchases_router)
 app.include_router(finance_router)
 app.include_router(sales_router)
 app.include_router(reports_router)
-#PASO NUMERO DESPUES DE LA MULTI REPARADO AUN BAJO OBSERVACION 
+
+
+# --------------------------------------------------
+# Startup
+# --------------------------------------------------
+@app.on_event("startup")
+def startup_event():
+    """
+    IMPORTANTE:
+    - NO ejecutar seeds aquí
+    - NO ejecutar migraciones aquí
+    - Startup debe ser SIEMPRE liviano y seguro
+    """
+    pass
